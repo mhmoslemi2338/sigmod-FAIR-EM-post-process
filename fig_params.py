28a65a4d14bf1c3fd31f292a16893d6947123076
moslemi <mhmoslemi2338@gmail.com>
2024-11-07 18:18:51 -0500

    add al

---
+++ fig_params.py
@@ -0,0 +1,16 @@
+tick_l = 10
+tick_w = 1.5
+F_ax = 25
+F_lbl = 35
+fig_size = (8,6)
+fig_alpha = 0.65
+F_legend = 28
+
+
+bar_fig_size =  (10, 7.8)
+bar_border_l = 2
+bar_yax_F = 40
+bar_xax_F = 36
+bar_legend_F = 35
+tick_l_bar = 14
+tick_w_bar = 2
